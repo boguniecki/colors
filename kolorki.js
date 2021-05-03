ebb6bcdbc71914b017dcdf211b1c2f01e161a411
boguniecki <marcin.boguniecki@gmail.com>
2021-05-03 16:21:54 +0200

    uproszczenie kodu

--- kolorki.js
+++ kolorki.js
@@ -1,8 +1,8 @@
-const background = document.getElementById("wrapper");
+const background = document.body;
 const colors = ["red", "green", "blue"];
 
 function changeColor() {    
     let color = colors[Math.floor(Math.random() * colors.length)];
-    
+
     background.style.backgroundColor = color;
 }
